4aedb1f8bff0c7af601b454e931f26b0316646b9
Fredy Rosero <faroseroc@unal.edu.co>
2025-03-08 12:19:36 -0500

    Fix earcut usage in Logic class to call the function directly

--- client-server/segregation-1/back/logic.class.js
+++ client-server/segregation-1/back/logic.class.js
@@ -122,7 +122,7 @@ class Logic {
     points.forEach(p => {
       coords.push(p.x, p.y);
     });
-    let indexTriangledPoints = earcut.default(coords);
+    let indexTriangledPoints = earcut(coords);
     for (let i = 0; i < indexTriangledPoints.length; i += 3) {
       let idx0 = indexTriangledPoints[i];
       let idx1 = indexTriangledPoints[i + 1];
