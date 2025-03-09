998b9868bfb877da9cf3329732e9240922f9ae9c
Fredy Rosero <faroseroc@unal.edu.co>
2025-03-08 22:55:05 -0500

    update config and text

--- 4-client-server-segregation-1/front/config.js
+++ 4-client-server-segregation-1/front/config.js
@@ -1,2 +1,3 @@
-// config.js
-window.SERVER_URL = "https://ec2-3-137-188-183.us-east-2.compute.amazonaws.com:3443";
+window.SERVER_URL = window.location.hostname === "localhost" 
+  ? "http://localhost:3000" 
+  : "https://ec2-3-137-188-183.us-east-2.compute.amazonaws.com:3443";
